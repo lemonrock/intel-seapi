228e3fac4c1f7f8d3bea740786f9ba30235f48df
Raphael Cohn <raphael.cohn@stormmq.com>
2018-10-25 17:41:35 +0100

    Cross compilation and musl support

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # intel-seapi
 
-[intel-seapi] is a FFI wrapper for the Intel Single Event API (SEAPI, also known as [IntelSEAPI]) and `ittnotify`, as used by Intel VTune.
+[intel-seapi] is a FFI wrapper for the Intel Single Event API (SEAPI, also known as [IntelSEAPI]) and `ittnotify`, as used by Intel VTune. ***This particular wrapper uses a fork of [IntelSEAPI] because the upstream code base doesn't support cross-compilation or the MUSL C library.***
 
 It provides a static link and generates Rust FFI bindings to the `libittnotify.a/.obj` library, and also compiles , but does not link or generate Rust bindings for, the`libIntelSEAPI.dylib/.so/.dll` dynamic library (this is because it is only ever built dynamically and because it is designed to be used from C++).
 
-Currently this crate is incapable of working with cross-compilation. This is a limitation of the underling CMake build system ussed by `IntelSEAPI`.
-
 Downstream crates can use the generated build variables `cargo:include`, `cargo:libdir` and `cargo:root`.
+
+
+## Limitations
+
+* JIT profiling is not yet supported;
+* The dynamic library `IntelSEAPI` is built but not linked and no bindings are generated for it.
+* May not be actively supported, as I only needed this functionality once for a project I was working on.
+
+
+## Cross-Compilation
+
+This is brittle, and may fail, mostly because the underlying software uses a mixture of CMake, a C++ toolchain with Rust bindings and wrappers, and [IntelSEAPI] is not cross-compile friendly. We use a forked version to fix some cross-compilation mistakes in their `CMakeLists.txt` files.
+
+Firstly, when compiling for a Mac OS X target, it is assumed the binary `lipo` is in the `PATH` as the native Mac OS X CMake build produces 'fat' archives which `rustc` can't handle. It tries to strip out the architecture of the target.
+
+Secondly, when compiling for MUSL targets, Rust's `cc` crate assumes the C compiler is `musl-gcc` and the C++ compiler if `musl-g++`. Since the `cc` crate is managed by the `cmake` crate, this is something we can't easy change. This crate attempts to set the environment variable `CROSS_COMPILE` if not set already when cross-compiling using a MUSL target.
+
+
+### Known cross-compilations that work.
+
+* On Mac OS X, with filo-sottie's foked musl homebrew keg installed (`lemonrock/musl-cross/musl-cross`), `cargo build --target x86_64-unknown-linux-musl`.
 
 
 ## Licensing
--- src/build.rs
+++ src/build.rs
@@ -8,29 +8,62 @@ extern crate cmake;
 
 use ::bindgen::*;
 use ::cmake::Config;
+use ::std::env::set_var;
 use ::std::env::var;
 use ::std::fs::create_dir;
 use ::std::fs::remove_dir;
+use ::std::path::Path;
 use ::std::path::PathBuf;
 
 
+const PROJECT_TREE: &'static str = "lib/IntelSEAPI";
+
+
 fn main()
+{	
+	let out_dir = getenv_unwrap("OUT_DIR");
+	
+	let install_folder_path = install_folder_path(&out_dir);
+	
+	create_missing_folders_that_cmakelists_requires();
+	
+	make_sure_cross_compilation_works_for_musl();
+	
+	cmake(&install_folder_path);
+	
+	let mislocated_bin_folder_path = mislocated_bin_folder_path(&out_dir);
+	
+	let include_folder_path = include_folder_path(&install_folder_path);
+	
+	print_cargo_path_value("root", &install_folder_path);
+	
+	print_cargo_path_value("include", &include_folder_path);
+	
+	print_cargo_path_value("libdir", &mislocated_bin_folder_path);
+	
+	print_cargo_path_value("rustc-link-search", &mislocated_bin_folder_path);
+		
+	print_cargo_rustc_link_lib(&mislocated_bin_folder_path);
+	
+	bindgen(&include_folder_path, &out_dir)
+}
+
+fn install_folder_path(out_dir: &str) -> PathBuf
 {
-	const PROJECT_TREE: &'static str = "lib/IntelSEAPI";
-	
-	let out_dir = var("OUT_DIR").unwrap();
-	
-	// Install path.
-	let mut install_folder_path = PathBuf::from(&out_dir);
+	let mut install_folder_path = PathBuf::from(out_dir);
 	install_folder_path.push("install");
-	
+
 	#[allow(unused_must_use)]
 	{
 		remove_dir(&install_folder_path);
 		create_dir(&install_folder_path);
 	}
 	
-	// The CMakeLists.txt for IntelSEAPI don't create these folders but depend on them and include them in .gitignore.
+	install_folder_path
+}
+
+fn create_missing_folders_that_cmakelists_requires()
+{
 	for missing_folder in ["bin", "obj"].iter()
 	{
 		let mut missing_folder_path = PathBuf::from(PROJECT_TREE);
@@ -41,59 +74,149 @@ fn main()
 			create_dir(&missing_folder_path);
 		}
 	}
+}
+
+fn make_sure_cross_compilation_works_for_musl()
+{
+	let host = getenv_unwrap("HOST");
+	let target = getenv_unwrap("TARGET");
 	
-	Config::new(PROJECT_TREE)
-		.define("CMAKE_INSTALL_PREFIX", &install_folder_path)
-		.build();
+	if host != target
+	{
+		let is_musl_which_cmake_and_cc_crates_make_a_mess_of_cross_compile_naming = match target.as_str()
+		{
+			"i586-unknown-linux-musl" | "i686-unknown-linux-musl" | "x86_64-unknown-linux-musl" => true,
+			_ => false,
+		};
+		
+		if is_musl_which_cmake_and_cc_crates_make_a_mess_of_cross_compile_naming && var("CROSS_COMPILE").is_err()
+		{
+			let mut cross_compile = target.replace("-unknown-linux", "-linux");
+			cross_compile.push('-');
+			set_var("CROSS_COMPILE", cross_compile)
+		}
+	}
+}
+
+fn cmake(install_folder_path: &Path)
+{
+	let mut config = Config::new(PROJECT_TREE);
 	
+	config.define("CMAKE_INSTALL_PREFIX", &install_folder_path);
+	config.very_verbose(true);
+
+	let host = getenv_unwrap("HOST");
+	let target = getenv_unwrap("TARGET");
+	let is_cross_compiling = host != target;
+	if is_cross_compiling
+	{
+		config.define("CMAKE_CROSSCOMPILING:BOOL", "ON");
+
+		let mut target_pieces = target.split('-');
+		target_pieces.next().unwrap();
+		
+		let cmake_system_name = match (target_pieces.next().unwrap(), target_pieces.next())
+		{
+			("apple", Some("darwin")) | ("apple", Some("ios")) => "Darwin",
+			("linux", Some("android")) | ("linux", Some("androideabi")) => "Android",
+			("none", Some(_)) | ("unknown", Some("none")) | ("unknown", Some("unknown")) => "Generic",
+			("rumprun", Some("netbsd")) | ("unknown", Some("netbsd")) => "NetBSD",
+			("unknown", Some("freebsd")) => "FreeBSD",
+			("unknown", Some("linux")) => "Linux",
+			("pc", Some("windows")) => "Windows",
+			("sun", Some("solaris")) => "SunOS",
+			
+			("unknown", Some("cloudabi")) => "Generic",
+			("unknown", Some("redox")) => "Generic",
+			("unknown", Some("emscripten")) => "Generic",
+			("unknown", Some("hermit")) => "Generic",
+			("fuchsia", None) => "Generic",
+			
+			_ => "Generic",
+		};
+		
+		config.define("CMAKE_SYSTEM_NAME", cmake_system_name);
+		
+
+		// There is a bug in the IntelSEAPI which means cross-compiling doesn't work.
+		
+		
+		
+		// if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+	}
 	
+	// CMAKE_OSX_ARCHITECTURES needs to be changed to avoid lipo.
+	
+	// Something is setting 'APPLE'.
+	
+	config.build();
+}
+
+fn mislocated_bin_folder_path(out_dir: &str) -> PathBuf
+{
 	// This bin folder is created by CMakeLists.txt in the wrong location, ignoring CMAKE_INSTALL_PREFIX!
-	let mut mislocated_bin_folder_path = PathBuf::from(&out_dir);
+	let mut mislocated_bin_folder_path = PathBuf::from(out_dir);
 	mislocated_bin_folder_path.pop();
 	mislocated_bin_folder_path.push("bin");
-	
-	
-	println!("cargo:root={}", install_folder_path.to_str().unwrap());
-	
+	mislocated_bin_folder_path
+}
+
+fn include_folder_path(install_folder_path: &Path) -> PathBuf
+{
 	let mut include_folder_path = PathBuf::from(install_folder_path);
 	include_folder_path.push("include");
-	println!("cargo:include={}", include_folder_path.to_str().unwrap());
+	include_folder_path
+}
 	
-	println!("cargo:libdir={}", mislocated_bin_folder_path.to_str().unwrap());
-	
-	println!("cargo:rustc-link-search=native={}", mislocated_bin_folder_path.to_str().unwrap());
+fn print_cargo_path_value(name: &str, path_value: &Path)
+{
+	println!("cargo:{}={}", name, path_value.to_str().unwrap());
+}
+
+fn print_cargo_rustc_link_lib(mislocated_bin_folder_path: &Path)
+{
+	let target = getenv_unwrap("TARGET");
+	let is_mac_os_x = target.ends_with("-darwin") || target.ends_with("-ios");
 	
 	// Rustc (1.31.0-nightly 2018-10-16) on Mac OS X fails to statically link the libittnotify.a library with an inaccurate error message of "error: failed to add native library ... File too small to be an archive".
 	// This is because it does not correctly recognise fat (multi-architecture) object archives.
-	// This fix is not cross-compile friendly.
-	if cfg!(target_os = "macos")
+	if is_mac_os_x
 	{
 		use ::std::process::Command;
-		
-		let mut original_archive_file_path = mislocated_bin_folder_path.clone();
+	
+		let mut original_archive_file_path = PathBuf::from(mislocated_bin_folder_path);
 		original_archive_file_path.push("libittnotify.a");
-		
-		let mut thin_archive_file_path = mislocated_bin_folder_path.clone();
+	
+		let mut thin_archive_file_path = PathBuf::from(mislocated_bin_folder_path);
 		thin_archive_file_path.push("libittnotify.a");
 		
-		Command::new("lipo").current_dir(mislocated_bin_folder_path.to_str().unwrap()).arg("libittnotify.a").arg("-thin").arg("x86_64").arg("-output").arg("libittnotify-thin.a").status().expect("failed to execute lipo");
+		let target_architecture = match target.split('-').next().unwrap()
+		{
+			"aarch64" => "arm64",
+			ok @ _ => ok,
+		};
 		
+		Command::new("lipo").current_dir(mislocated_bin_folder_path.to_str().unwrap()).arg("libittnotify.a").arg("-thin").arg(target_architecture).arg("-output").arg("libittnotify-thin.a").status().expect("failed to execute `lipo`");
+	
 		println!("cargo:rustc-link-lib=static=ittnotify-thin");
 	}
 	else
 	{
 		println!("cargo:rustc-link-lib=static=ittnotify");
 	}
-	
-	let mut header_filer_path = include_folder_path.clone();
+}
+
+fn bindgen(include_folder_path: &Path, out_dir: &str)
+{
+	let mut header_filer_path = PathBuf::from(include_folder_path);
 	header_filer_path.push("ittnotify.h");
-	
+
 	let mut rust_fmt_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 	rust_fmt_file_path.push("rustfmt.toml");
-	
-	let mut bindings_file_path = PathBuf::from(&out_dir);
+
+	let mut bindings_file_path = PathBuf::from(out_dir);
 	bindings_file_path.push("ittnotify.bindgen.rs");
-	
+
 	Builder::default()
 		.header(header_filer_path.to_str().unwrap())
 		.ctypes_prefix("::libc")
@@ -118,9 +241,19 @@ fn main()
 		.whitelist_function("__itt.*")
 		.whitelist_type("__itt.*")
 		.whitelist_var("__itt.*")
-		
+	
 		.generate()
 		.expect("Unable to generate bindings for ittnotify.h")
 		.write_to_file(bindings_file_path)
-		.expect("Could not write bindgen bindings for ittnotify.h to ittnotify.bindgen.rs")
+	.expect("Could not write bindgen bindings for ittnotify.h to ittnotify.bindgen.rs")
+}
+
+fn getenv_unwrap(environment_variable_name: &str) -> String
+{
+    match var(environment_variable_name)
+	{
+        Ok(value) => value,
+		
+        Err(..) => panic!(format!("environment variable `{}` not defined", environment_variable_name)),
+    }
 }
